4dd457e57f163a05f91057202f5c97e52cd4ce1b
LuisSFU <Saloch_1s@hotmail.com>
2019-09-26 16:23:45 -0500

    Fix[name]: Migration names

--- db/migrate/20190924003738_create_logs.rb
+++ db/migrate/20190924003738_create_logs.rb
@@ -1,7 +1,7 @@
 class CreateLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :logs do |t|
-      t.references :pacient_medical, foreign_key: true
+      t.references :patient_medical, foreign_key: true
       t.float :value
 
       t.timestamps
