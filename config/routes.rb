a640c96b1fc143a4b26faaee16c9da30647096f0
Luis Carlos Figueroa Rodríguez <lc.figueroar26@gmail.com>
2019-09-28 16:55:19 -0500

    Devise Doctor user added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :patients
+  devise_for :doctors, path: 'doctors'
+  devise_for :patients, path: 'patients'
+
+  resources :doctors
+  resources :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
