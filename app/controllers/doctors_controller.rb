2b7c9bc80f66fd44424aba570f5cd875cc13dfbc
Luis Carlos <lc.figueroar26@gmail.com>
2019-10-03 16:21:45 -0500

    Feat/update dr info (#44)
    * Doctors can update only their info
    
    * error message of email taken added
    
    * Code climate fixes

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,11 +1,16 @@
 # Doctor Controller controlls or actions regarding Doctor User Types
 
 class DoctorsController < ApplicationController
+  before_action :validate_permission, only: %i[edit update destroy show]
 
   def index; end
 
   def new
     @doctor = Doctor.new
+  end
+
+  def edit
+    @doctor = Doctor.find(params[:id])
   end
 
   def create
@@ -19,8 +24,19 @@ class DoctorsController < ApplicationController
     end
   end
 
-  def show
+  def update
     @doctor = Doctor.find(params[:id])
+    if @doctor.update(doctor_params)
+      flash[:success] = 'Información actualizada exitosamente'
+      redirect_to @doctor
+    else
+      flash.now[:error] = 'Hubo un error con el registro, verifica los campos del formulario'
+      render :edit
+    end
+  end
+
+  def show
+    @doctor = current_doctor
     @patients = @doctor.patients
     @viewable_patients = @doctor.viewable_patients
   end
@@ -36,5 +52,9 @@ class DoctorsController < ApplicationController
 
   def doctor_params
     params.require(:doctor).permit(:first_name, :last_name, :birth_date, :sex, :email, :password, :password_confirmation)
+  end
+
+  def validate_permission
+    redirect_to root_path if current_doctor.id.to_s != params[:id]
   end
 end
