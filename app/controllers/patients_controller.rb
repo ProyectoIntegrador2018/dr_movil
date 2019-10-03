d7107b3a4f2d0db0aa1efe7028e6507e5955a904
Luis Carlos <lc.figueroar26@gmail.com>
2019-10-03 16:21:28 -0500

    Patients can update their info (#45)
    * Patients can update their info
    
    * Code Climate fixes

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,23 +1,40 @@
 
 class PatientsController < ApplicationController
   before_action :authenticate_doctor!, only: [:new, :create]
-  before_action :check_dotor
+  before_action :check_doctor, except: %i[index]
 
   def index
     @patients = Patient.all
   end
 
   def new
-    @patient = Patient.new()
+    @patient = Patient.new
+  end
+
+  def edit
+    @patient = Patient.find(params[:id])
   end
 
   def create
     @patient = Patient.new(patient_params)
     if @patient.save
-      flash[:success] = "Paciente registrado exitosamente"
+      flash[:success] = 'Paciente registrado exitosamente'
       redirect_to patients_path
     else
-      flash.now[:error] = "Hubo un error con el registro, verifica los campos del formulario"
+      flash.now[:error] = 'Hubo un error con el registro,
+                           verifica los campos del formulario'
+      render :new
+    end
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+    if @patient.update(patient_params)
+      flash[:success] = 'Información actualizada exitosamente'
+      redirect_to @patient
+    else
+      flash.now[:error] = 'Hubo un error con el registro,
+                           verifica los campos del formulario'
       render :new
     end
   end
@@ -39,7 +56,7 @@ class PatientsController < ApplicationController
     params.require(:patient).permit(:first_name, :last_name, :birth_date, :sex, :email, :password, :password_confirmation)
   end
 
-  def check_dotor
+  def check_doctor
     @patient = Patient.find(params[:id])
     unless (current_patient.nil? && current_doctor.has_patient(@patient) || @patient.doctor == current_doctor) || current_patient == @patient
       redirect_to root_path
