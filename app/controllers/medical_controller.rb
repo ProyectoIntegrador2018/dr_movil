47e308c86ea60fe3e0a8d9d4791a37f1667ced4a
LuisSFU <Saloch_1s@hotmail.com>
2019-10-16 10:31:07 -0500

    Feat[Medical]: Add update field to table

--- app/controllers/medical_controller.rb
+++ app/controllers/medical_controller.rb
@@ -2,9 +2,15 @@
 
 class MedicalController < ApplicationController
   before_action :check_doctor
+
   def index
     @medicals = PatientMedical.where(patient: params[:patient])
     @patient = Patient.find(params[:patient])
+  end
+
+  def update
+    PatientMedical.find(params[:medical]).update_attribute(:freq, params[:freq])
+    redirect_to action: "index", patient: params[:patient]
   end
 
   def check_doctor
