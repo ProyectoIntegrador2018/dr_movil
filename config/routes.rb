0800e9c04ea619f8a02879d35d3e88403aef2b10
Luis Carlos Figueroa Rodríguez <lc.figueroar26@gmail.com>
2019-10-17 17:25:26 -0500

    Patients admin permissions can be admin from views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-   get '/medical', to: 'medical#index'
-   patch '/medical', to: 'medical#update'
+  get '/medical', to: 'medical#index'
+  patch '/medical', to: 'medical#update'
   devise_for :doctors, path: 'doctors', controllers: { sessions: "doctors/sessions" }
   devise_for :patients, path: 'patients', controllers: { sessions: "patients/sessions" }, skip: [:registrations]
 
