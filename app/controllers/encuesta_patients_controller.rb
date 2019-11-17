c3c4c992cd463962b1e634f2a854c0d798a50dee
Rubén Garza <ruben.garza@bimset.mx>
2019-11-17 11:56:45 -0600

    Encuesta controller update and some views

--- app/controllers/encuesta_patients_controller.rb
+++ app/controllers/encuesta_patients_controller.rb
@@ -7,10 +7,17 @@ class EncuestaPatientsController < ApplicationController
     @encuesta.encuesta_id = params[:encuesta_id]
     if @encuesta.save
       flash[:success] = 'Encuesta relacionada exitosamente.'
-      redirect_to encuestas_path
+      redirect_to encuesta_path(params[:encuesta_id])
     else
       flash[:success] = "Hubo un error con el registro, contacte al administrador."
       redirect_to encuestas_path
     end
+  end
+
+  def destroy
+    @encuesta = EncuestaPatient.where(encuesta_id: params[:encuesta_id], patient_id: params[:id]).first
+    @encuesta.destroy
+    flash[:destroy] = 'La encuesta ha sido removida de la vista del paciente.'
+    redirect_to encuesta_path(params[:encuesta_id])
   end
 end
