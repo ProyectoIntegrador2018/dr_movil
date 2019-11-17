63a9e539eea1108445726f21438cf662c54b6ad6
Luis Carlos <lc.figueroar26@gmail.com>
2019-11-17 10:24:39 -0600

    Feat/print records (#59)
    * Print page
    
    * Status of log added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_025119) do
+ActiveRecord::Schema.define(version: 2019_11_16_233448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_025119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "patient_medical_id"
+    t.string "status"
     t.index ["patient_medical_id"], name: "index_logs_on_patient_medical_id"
   end
 
