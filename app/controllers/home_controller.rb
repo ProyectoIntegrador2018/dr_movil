16b7eca53a7a5eb35e98126bcf11725f5f676679
Luis Carlos <lc.figueroar26@gmail.com>
2019-11-19 20:33:29 -0600

    Enhance/frontend views (#66)
    * Medical Variables delete button removed
    
    * Home updated
    
    * Log In Doctor logo added
    
    * Patient login logo added
    
    * Log in redirections
    
    * Profile is home
    
    * Alerts style updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_patient.nil? && current_doctor
+      redirect_to current_doctor
+    elsif current_patient && current_doctor.nil?
+      redirect_to current_patient
+    end
   end
 end
