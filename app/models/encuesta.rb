5a470d7da4da16bc2d0f22b0175cf1aa61a56c09
rbngza <ruben_garzah@hotmail.com>
2019-12-01 14:12:56 -0600

    Fix for encuesta destroy (#74)

--- app/models/encuesta.rb
+++ app/models/encuesta.rb
@@ -1,5 +1,5 @@
 class Encuesta < ApplicationRecord
   belongs_to :doctor
-  has_many :encuesta_patient
+  has_many :encuesta_patient, dependent: :destroy
   has_many :patients, through: :encuesta_patient
 end
